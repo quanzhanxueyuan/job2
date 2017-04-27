b6b8d9931d9e6e99197ebb7fe1b4b67d4e48a601
panyujin <panyujinyx@gmail.com>
2017-04-27 17:22:34 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,8 @@
-class JobsController < ApplicationController
+class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
+
+
   def show
     @job = Job.find(params[:id])
   end
@@ -16,7 +19,7 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
 
     if @job.save
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :new
     end
@@ -29,7 +32,7 @@ class JobsController < ApplicationController
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
       render :edit
     end
@@ -40,9 +43,8 @@ class JobsController < ApplicationController
 
     @job.destroy
 
-    redirect_to jobs_path
+    redirect_to admin_jobs_path
   end
-
 
   private
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  def require_is_admin
+    if current_user.email != '1@1.com'
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
 end
